fcdcd84757a55e3df14a629adbd28fe09f66bf5c
James Gan <jg893@cornell.edu>
2019-03-12 21:06:24 -0500

    qloo and route for it :)

--- flask_app/example.py
+++ flask_app/example.py
@@ -1,15 +1,19 @@
-from flask import Flask
+from flask import Flask, jsonify
+from qloo import *
 app = Flask(__name__)
+# allows use of lists with syntax /Artist+Artist+Artist/
+from util import ListConverter
+app.url_map.converters['list'] = ListConverter
 
 @app.route('/')
 def index():
     return 'Index Page'
 
-@app.route('/hello')
-def hello():
-    return 'Hello, World'
+@app.route('/qloo/<list:artists>/<longitude>/<latitude>/<radius>')
+def qloo(artists, longitude, latitude, radius):
+    print(artists)
+    return jsonify(get_qloo(artists, longitude + ',' + latitude, radius))
 
 
 if __name__ == '__main__':
     app.run()
-
